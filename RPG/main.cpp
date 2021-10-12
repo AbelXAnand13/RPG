86347616e4eacf671018a1360d840f0506d63b5f
Abel <abelanand13@gmail.com>
2021-10-11 20:22:33 -0700

    hola

--- RPG/main.cpp
+++ RPG/main.cpp
@@ -1,14 +1,77 @@
-//
-//  main.cpp
-//  RPG
-//
-//  Created by Abel Christy Anand on 10/11/21.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <string>
+#include <stdio.h>
+#include <chrono>
+#include <thread>
+#include <unistd.h>
+#include <time.h>
+using namespace std;
+//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
+void weaponsMenu() {
+    cout << "WEAPONS MENU\n";
+    cout << "1. KATANA: Damage-100   Range-1   Ammo-INFINITE   Fire-Rate-20\n";
+    cout << "2. CATACLYST: Damage-20   Range-75   Ammo-20   Fire-Rate-75   Type-SMG\n";
+    cout << "3. 50_CAL: Damage-100   Range-100   Ammo-6   Fire-Rate-1   Type-SNIPER\n";
+    cout << "4. AK_47: Damage-40   Range-80   Ammo-30   Fire-Rate-45   Type-AR\n";
+    cout << "Choose your Weapon\n";
+}
+int main()
+{
+    string name;
+    printf("Cyberspace ----- Shoutout to READY PLAYER ONE for inspiring this Game\n");
+    printf("Enter your Name to continue...\n");
+    cin >> name;
+    cout << "Welcome " << name << "\n";
+    cout << "Chapter 1: INTRO\n";
+    sleep(2);
+    cout << "You are nobody and nothing you do matters...\n";
+    sleep(2);
+    cout << "In the year 2214 everything you need is virtual...\n";
+    sleep(2);
+    cout << "You are fed up with your life... \n";
+    sleep(2);
+    cout << "Every day of your life is a loop...\n";
+    sleep(2);
+    cout << "The only way you can change your life around is if you can make some money...\n";
+    sleep(2);
+    cout << "Back in 2200 the leading Tech Giant set up a contest with a prize no one could reject-\n";
+    sleep(2);
+    cout << "51% of their stock and 1 trillion dollars... \n";
+    sleep(2);
+    cout << "If you win the contest, you get the prize\n";
+    sleep(2);
+    cout << "How hard can it be... \n";
+    sleep(2);
+    cout << "To win the contest, you need to find a cube...\n";
+    sleep(2);
+    cout << "As easy as it is, no one has found the cube...\n";
+    sleep(2);
+    cout << "You don't know it, but today is a special day...\n";
+    sleep(2);
+    cout << "As you go to the grocery store after 8 hours of virtual school, Don, the cashier greets you\n";
+    sleep(2);
+    cout << "You buy your weekly supply of soda and go back home\n";
+    sleep(2);
+    cout << "There were no clues to the cube except that it's hidden in Cyberspace\n";
+    sleep(2);
+    cout << "Cyberspace is a virtual game much like the Oasis from the movie Ready Player One, a very old movie.\n";
+    sleep(2);
+    cout << "It's about Wade, a boy who wins a lot of money by finding a few keys.\n";
+    sleep(2);
+    cout << "Everyone aspires to be like him and find the cube in Cyberspace\n";
+    sleep(2);
+    cout << "3 years ago, the creators of cyberspace gave a clue to find the cube.\n";
+    sleep(2);
+    cout << "\"To find the cube, you need to do something you will never do.\"\n";
+    sleep(2);
+    cout << "There is an in-game leaderboard that's been empty for the 14 years since the contest started\n";
+    sleep(2);
+    cout << "You've dreamt of being the first one on it.\n";
+    sleep(2);
+    cout << "Chapter 2: CYBERSPACE\n";
+    sleep(2);
+    cout << " Unike many other games, you can't actually win in Cyberspace. The point is to survive\n";
+    sleep(2);
+    cout << "Wait, survive, the main point of the game is to survive.\n";
+    sleep(2);
 }
